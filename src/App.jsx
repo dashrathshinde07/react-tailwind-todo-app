a55a733834cc8d1a7fa067618a7359e596cb673a
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-06-22 13:26:51 +0530

    Complete UI implementation using Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Todo from "./components/Todo";
 
 const App = () => {
   return (
     <>
-      <div>App</div>
+      <div className="bg-stone-900 grid py-4 min-h-screen">
+        <Todo />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
